3b3b0d2d9205c6815db7d863374b0d03b97fb031
coolmarie <coolmarie082@gmail.com>
2025-10-27 13:11:23 -0700

    Update V5__insert_article_views.sql

--- src/main/resources/db/migration/V5__insert_article_views.sql
+++ src/main/resources/db/migration/V5__insert_article_views.sql
@@ -1,25 +1,19 @@
--- Users table
 CREATE TABLE users (
                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
                       user_id VARCHAR(255) NOT NULL
 );
 
--- Articles table
 CREATE TABLE article (
                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
                          article_id VARCHAR(255) NOT NULL,
                          description VARCHAR(255)
 );
 
--- Article views table\
 CREATE TABLE article_view (
                               id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                              user_id VARCHAR(255) NOT NULL,
                               article_id BIGINT NOT NULL,
-                              user_id BIGINT NOT NULL,
-                              viewed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                              CONSTRAINT uq_user_article UNIQUE (article_id, user_id),
-                              CONSTRAINT fk_article FOREIGN KEY (article_id) REFERENCES article(id) ON DELETE CASCADE,
-                              CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                              views INT DEFAULT 0,
+                              UNIQUE KEY unique_user_article (user_id, article_id),
+                              FOREIGN KEY (article_id) REFERENCES article(id) ON DELETE CASCADE
 );
-
-
